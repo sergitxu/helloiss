2849a5be0d6fe33006ccd5bf0f490096453c4c0a
Sergitxu <sergiortiz@gmail.com>
2019-02-06 00:16:26 +0100

    video changed to Youtube News space

--- public/js/helloiss.js
+++ public/js/helloiss.js
@@ -88,81 +88,6 @@ setInterval(
     `;
   
   }, 1000);
-  
-
-//Get ISS news
-/*
-let getISSNews = url => {
-  var xmlHttp = new XMLHttpRequest();
-  
-  xmlHttp.onreadystatechange = function() {
-
-      if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-        let getNews = JSON.parse(xmlHttp.responseText);
-
-        console.log(getNews);
-        
-        // if (getNews.geonames[0]) {
-        //   let ISScountryCode = getNews.geonames[0].countryCode;
-        //   let ISScountryName = getNews.geonames[0].countryName;
-        //   document.getElementById('countryCode').innerText = `${ISScountryName}: ${ISScountryCode}`;
-        // }
-        
-
-      } else if (xmlHttp.readyState === 4 && xmlHttp.status === 404) {
-          console.error("ERROR! 404");
-          console.info(JSON.parse(xmlHttp.responseText));
-      }
-  };
-  xmlHttp.open("GET", url, true);
-  xmlHttp.send();
-}
-// Create the XHR object.
-function createCORSRequest(method, url) {
-  var xhr = new XMLHttpRequest();
-  if ("withCredentials" in xhr) {
-    // XHR for Chrome/Firefox/Opera/Safari.
-    xhr.open(method, url, true);
-  } else if (typeof XDomainRequest != "undefined") {
-    // XDomainRequest for IE.
-    xhr = new XDomainRequest();
-    xhr.open(method, url);
-  } else {
-    // CORS not supported.
-    xhr = null;
-  }
-  return xhr;
-}
-// Helper method to parse the title tag from the response.
-function getTitle(text) {
-  return text.match('<title>(.*)?</title>')[1];
-}
-// Make the actual CORS request.
-function makeCorsRequest() {
-  // This is a sample server that supports CORS.
-  var url = 'http://html5rocks-cors.s3-website-us-east-1.amazonaws.com/index.html';
-
-  var xhr = createCORSRequest('GET', url);
-  if (!xhr) {
-    alert('CORS not supported');
-    return;
-  }
-
-  // Response handlers.
-  xhr.onload = function() {
-    var text = xhr.responseText;
-    var title = getTitle(text);
-    alert('Response from CORS request to ' + url + ': ' + title);
-  };
-
-  xhr.onerror = function() {
-    alert('Woops, there was an error making the request.');
-  };
-
-  xhr.send();
-}
-
-*/
 
 function sayHello() {
 
@@ -195,7 +120,6 @@ function sayHello() {
 function init() {
   
   getCrew();
-  // getISSNews("https://blogs.nasa.gov/spacestation/feed/");
   sayHello();
 }
 
