e7120238cce39a58f8bb73f50576d896d88c0d3f
Sergitxu <sergiortiz@gmail.com>
2019-02-06 21:14:47 +0100

    gulp watcher added

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,13 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-
 gulp.task('sassCompile', async function() {
     gulp.src('sass/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('public/css/'));
+});
+
+// add watcher
+gulp.task('default', function() {
+    gulp.watch('sass/*.scss', gulp.series('sassCompile'));
 });
